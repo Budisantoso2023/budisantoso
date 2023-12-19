1ca1494cd66eb5c2d9404ec0ccc3b13f4591a75b
Budisantoso2023 <146050753+Budisantoso2023@users.noreply.github.com>
2023-12-19 12:52:18 +0800

    19-12-2023

---
+++ IFELSE_TUGAS_ALGORITMA/src/BanyakIf.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+    
+
+public class BanyakIF {}
+public static void main(String[] args)
+    // Data Member
+    int nilai;
+    String grade;
+
+    // membuat objek Scanner
+    Scanner scan = new Scanner(System.in);
+
+    // memasukkan input dari keybord
+    System.out.print(s:"Masukkan Nilai : "); 
+    nilai = scan.nextInt();
+
+    // IF Statement
+    if ( nilai  >= 80 ) {
+        grade = "A";
+        } else if  (nilai >= 70 && nilai <= 79 ) {
+            grade = "B";
+        } else if ( nilai >= 60 && nilai <= 69 )  {
+            
+            
+     System.out.println(Angka : ""     
+        
+    }
+    
+
+
+
+
---
+++ IFELSE_TUGAS_ALGORITMA/src/IFElse.java
@@ -0,0 +1,24 @@
+import com.ibm.security.krb5.internal.crypto.x;
+
+public class App  {
+    public static void main(String[] args) throws Exception {
+       
+        // Data member
+        int total_belanja = 0;
+
+        // membuat scanner
+        Scanner scan = new Scanner(System.in);
+
+        // mengambil input dari keybord
+        System.out.println(x:"Anda mendapatkan mouse");
+     } else { )
+        System.out.println(x:"Belanja anda kurang dari rp : 50000");
+
+
+        System.out.println(x:"TerimaKasih sudah belanja di toko kami");
+          
+
+         
+
+    }
+}
